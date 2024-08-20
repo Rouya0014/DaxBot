1051b68ed91d618f3fc6e455c5444d382e47a13c
_Rouya <theo.toupin@outlook.com>
2024-08-20 23:49:29 +0200

    Add files via upload

--- commands/message/General/ping.js
+++ commands/message/General/ping.js
@@ -1,1 +1,17 @@
+const { EmbedBuilder } = require('discord.js');
 
+module.exports = {
+    name: "ping",
+    type: 3,
+    run: async (client, interaction, config, db) => {
+        await interaction.deferReply();
+
+        const reply = await interaction.fetchReply();
+    
+        const ping = reply.createdTimestamp - interaction.createdTimestamp;
+    
+        interaction.editReply(
+          `Pong! Client ${ping}ms | Websocket: ${client.ws.ping}ms`
+        );
+    },
+};
