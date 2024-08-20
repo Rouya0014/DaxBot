043b2adde1fefa017ba477a03c4302dbd7532612
_Rouya <theo.toupin@outlook.com>
2024-08-20 21:51:15 +0200

    Add files via upload

--- events/Music/music.js
+++ events/Music/music.js
@@ -148,10 +148,10 @@ client.on("interactionCreate", (interaction) => {
       })
       .addFields({ name: "Vidéo", value: "[Cliquez](" + url + ")", inline: true })
       .setColor("5865f2")
-        .setImage(`${thumb}`);
+      .setImage(`${thumb}`);
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setEmoji("<:PlayIcon:1100757469308923996>")
+          .setEmoji("<:ResumeIcon:1184958276186677309>")
           .setStyle(ButtonStyle.Danger)
           .setCustomId("devam")
       );
@@ -291,25 +291,25 @@ client.on("interactionCreate", (interaction) => {
         .setImage(`${thumb}`);
       const row = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setEmoji("<:PauseIcon:1100757461192937492>")
+          .setEmoji("<:PauseIcon:1184957491528867942>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("dur"),
         new ButtonBuilder()
-          .setEmoji("<:VolumeIcon:1100760795522015334>")
+          .setEmoji("<:ScreenShareVolumeIcon:1184958280188043335>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("volume"),
         new ButtonBuilder()
-          .setEmoji("<:SkipIcon:1100757452795936820>")
+          .setEmoji("<:SkipIcon:1266807444391067778>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("skip"),
         new ButtonBuilder()
-          .setEmoji("<:BoucleIcon:1100757483049463839>")
+          .setEmoji("<:ReplayIcon:1184957488806760570>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("loop"),
       );
       const row2 = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
-          .setEmoji("<:MusicIcon:1088231735348703342>")
+          .setEmoji("<:HelpIcon:1184957471740141588>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("soru"),
         new ButtonBuilder()
@@ -317,11 +317,11 @@ client.on("interactionCreate", (interaction) => {
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("bassboost"),
         new ButtonBuilder()
-          .setEmoji("<:RewindIcon:1100763652635754596>")
+          .setEmoji("<:SlowModeIcon:1184957504665428058>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("slowmode"),
         new ButtonBuilder()
-          .setEmoji("<:FastForwardIcon:1100763640010907738>")
+          .setEmoji("<:SpeedUpIcon:1266808818457641051>")
           .setStyle(ButtonStyle.Secondary)
           .setCustomId("fast"),
       );
