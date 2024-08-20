040569b623f91d1c881e4ac1ead0eb43e5bda03b
_Rouya <theo.toupin@outlook.com>
2024-08-20 21:48:30 +0200

    Delete models/youtube.js

--- models/youtube.js
+++
@@ -1,8 +0,0 @@
-const {model, Schema} = require("mongoose");
-
-let youtubeSchema = new Schema({
-    GuildID: String,
-    lastVideoId: String,
-})
-
-module.exports = model("youtube", youtubeSchema)
